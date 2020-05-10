62542898a50d52d1a23c6cf48c554f5831e48b23
hsz <hsz14245@ly.com>
2020-05-10 22:08:32 +0800

    eureka

--- cloud2020/cloud-eureka-server7001/src/main/java/com/hsz/springcloud/EurekaMain7001.java
+++ cloud2020/cloud-eureka-server7001/src/main/java/com/hsz/springcloud/EurekaMain7001.java
@@ -2,15 +2,15 @@ package com.hsz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * @author created by hsz 2020/5/9 14:02
+ * Created by TANG on 2020/5/10 13:27
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
--- cloud2020/cloud-provider-payment8001/src/main/java/com/hsz/springcloud/PaymentMain8001.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/hsz/springcloud/PaymentMain8001.java
@@ -3,11 +3,13 @@ package com.hsz.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author created by hsz 2020/5/9 10:09
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
